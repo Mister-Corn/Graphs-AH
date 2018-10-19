af40892f5f06c59b55c43872dfaf389e30ffd070
kelly <kelfro@fastmail.com>
2018-10-18 17:05:19 -0700

    from Tues

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,42 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, value):
+        if value in self.vertices:
+            raise Exception("Vertex already exists")
+        else:
+            self.vertices[value] = set() 
+
+    def add_edge(self, vertex1, vertex2, bidirectional=True):
+        if vertex1 not in self.vertices or vertex2 not in self.vertices:
+            raise Exception("you cannot put an edge on nonexistent vertex")
+        else:
+            self.vertices[vertex1].add(vertex2)
+            if bidirectional:
+                self.vertices[vertex2].add(vertex1)
+    def dfs(self, starting_node, visited=None):
+        # Mark the node as visited
+        if visited is None:
+            # quese of visited nodes
+            visited = []
+        visited.append(starting_node)
+        # For each child, if that child hasn't been visited, call dft() on that node
+        for node in self.vertices[starting_node]:
+            if node not in visited:
+                self.dfs(node, visited)
+        return visited
+        # for child in children:
+        #    if child not in visited:
+        # dft(child, visted)
+
+#graph = Graph()  # Instantiate your graph
+#graph.add_vertex('0')
+#graph.add_vertex('1')
+#graph.add_vertex('2')
+#graph.add_vertex('3')
+#graph.add_edge('0', '1')
+#graph.add_edge('0', '3')
+#graph.add_edge('0', '4')
+#print(graph.vertices)
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -3,14 +3,29 @@
 """
 Demonstration of Graph and BokehGraph functionality.
 """
-
+import random
 from sys import argv
+from graph import Graph
+from draw import BokehGraph
 
 
-def main():
-    pass  # TODO
-
+def main(v, e):
+    graph = Graph()
+    for i in range(v):
+        graph.add_vertex(i)
+    for j in range(e):
+        random_vertex = int(random.random() * v)
+        random_edge = int(random.random() * e)
+        print(random_vertex, random_edge)
+        graph.add_edge(random_vertex, random_edge)
+    print(graph.vertices)
+    bg = BokehGraph(graph)
+    bg.show()
 
 if __name__ == '__main__':
-    # TODO - parse argv
-    main()
+    if len(argv) == 3:
+        v = argv[1]
+        e = argv[2]
+        main(int(v),int(e))
+    else:
+        main(5,5)
